f5957fed2b47894bf27f4fbb54055aa3e236b37b
Eduardo Ferrán <arterium_difusion@hotmail.com>
2024-02-01 18:36:03 -0600

    third commit

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -11,91 +11,106 @@ const QuioscoProvider = ({ children }) => {
   const [producto, setProducto] = useState({});
   const [modal, setModal] = useState(false);
   const [pedido, setPedido] = useState([]);
-  const [nombre, setNombre] = useState('');
+  const [nombre, setNombre] = useState("");
   const [total, setTotal] = useState(0);
 
   const router = useRouter();
 
-
   const obtenerCategorias = async () => {
-    const { data } = await axios('/api/categorias')
-    setCategorias(data)
-  }
+    const { data } = await axios("/api/categorias");
+    setCategorias(data);
+  };
 
   //Renderizar todas las categorías
   useEffect(() => {
-    obtenerCategorias()
-  }, [])
+    obtenerCategorias();
+  }, []);
 
   useEffect(() => {
-    const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
+    const nuevoTotal = pedido.reduce(
+      (total, producto) => producto.precio * producto.cantidad + total,
+      0
+    );
 
-    setTotal(nuevoTotal)
-  }, [pedido])
+    setTotal(nuevoTotal);
+  }, [pedido]);
 
-  //Selecionar categoría por default, 
+  //Selecionar categoría por default,
   //al inicio de la carga
   useEffect(() => {
-    setCategoriaActual(categorias[0])
-  }, [categorias])
+    setCategoriaActual(categorias[0]);
+  }, [categorias]);
 
-  const handleClickCategoria = id => {
-    const categoria = categorias.filter(cat => cat.id === id)
-    setCategoriaActual(categoria[0])
-    router.push('/')
-  }
+  const handleClickCategoria = (id) => {
+    const categoria = categorias.filter((cat) => cat.id === id);
+    setCategoriaActual(categoria[0]);
+    router.push("/");
+  };
 
-  const handleSetProducto = producto => {
-    setProducto(producto)
-  }
+  const handleSetProducto = (producto) => {
+    setProducto(producto);
+  };
 
   const handleChangeModal = () => {
-    setModal(!modal)
-  }
+    setModal(!modal);
+  };
 
   const handleAgregarPedido = ({ categoriaId, ...producto }) => {
-    if (pedido.some(productoState => productoState.id === producto.id)) {
+    if (pedido.some((productoState) => productoState.id === producto.id)) {
       //Actualizar cantidad de los productos
-      const pedidoActualizado = pedido.map(productoState => productoState.id === producto.id ? producto : productoState)
-      setPedido(pedidoActualizado)
+      const pedidoActualizado = pedido.map((productoState) =>
+        productoState.id === producto.id ? producto : productoState
+      );
+      setPedido(pedidoActualizado);
 
-      toast.success('Guardado correctamente')
-
+      toast.success("Guardado correctamente");
     } else {
-      setPedido([...pedido, producto])
-      toast.success('Agregado al pedido')
+      setPedido([...pedido, producto]);
+      toast.success("Agregado al pedido");
     }
 
     setModal(false);
-  }
+  };
 
-  const handleEditarCantidades = id => {
-    const productoActualizar = pedido.filter(producto => producto.id === id)
-    setProducto(productoActualizar[0])
+  const handleEditarCantidades = (id) => {
+    const productoActualizar = pedido.filter((producto) => producto.id === id);
+    setProducto(productoActualizar[0]);
     setModal(!modal);
-  }
+  };
 
-  const handleEliminarProducto = id => {
-    const pedidoActualizado = pedido.filter(producto => producto.id !== id)
-    setPedido(pedidoActualizado)
-  }
+  const handleEliminarProducto = (id) => {
+    const pedidoActualizado = pedido.filter((producto) => producto.id !== id);
+    setPedido(pedidoActualizado);
+  };
 
   const colocarOrden = async (e) => {
     e.preventDefault();
 
     try {
-      const { data } = await axios.post('/api/ordenes', { pedido, nombre, total, fecha: Date.now().toString() })
-      console.log(data)
+      await axios.post("/api/ordenes", {
+        pedido,
+        nombre,
+        total,
+        fecha: Date.now().toString(),
+      });
+
+      //Resetear la orden
+      setCategoriaActual(categorias[0])
+      setPedido([])
+      setNombre("")
+      setTotal(0)
+
+      toast.success('Pedido realizado correctamente')
+
+      //Regresar a la página de inicio
+      setTimeout(() => {
+        router.push('/')
+      }, 3000)
+
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
-    console.log(pedido);
-    console.log(nombre);
-    console.log(total);
-
   };
-
 
   return (
     <QuioscoContext.Provider
@@ -114,16 +129,13 @@ const QuioscoProvider = ({ children }) => {
         nombre,
         setNombre,
         colocarOrden,
-        total
-      }}
-    >
+        total,
+      }}>
       {children}
     </QuioscoContext.Provider>
-  )
-}
+  );
+};
 
-export {
-  QuioscoProvider
-}
+export { QuioscoProvider };
 
 export default QuioscoContext;
--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -1,18 +1,30 @@
 import { PrismaClient } from "@prisma/client";
 
+
+//Insertar pedido a la BD mediante PrismaClient
+
 export default async function handler(req, res) {
 const prisma = new PrismaClient();
 
+//Obteniendo órdenes en DB
+  const ordenes = await prisma.orden.findMany({
+    where: {
+      estado: false
+    }
+  })
+  res.status(200).json(ordenes);
+
+//Creando órdenes en DB
   if (req.method === "POST") {
     const orden = await prisma.orden.create({
       data: {
         nombre: req.body.nombre,
         total: req.body.total,
-        pedido: req.body.pedido
-      }
-    })
+        pedido: req.body.pedido,
+        fecha: req.body.fecha
+      },
+    });
 
-    console.log(req.body);
-    res.json({hola: 'mundo'});
+    res.status(200).json(orden);
   }
 }
